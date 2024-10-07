b62645df43cbee6f24751b72374f06ab4abf4716
Stephen Muñez <sjtmunez@gmail.com>
2024-10-07 18:37:50 +0800

    feat: capture all anchor clicks

--- orchard-fed-test_1/main.js
+++ orchard-fed-test_1/main.js
@@ -1,1 +1,9 @@
 import 'normalize.css';
+
+const anchorTags = document.querySelectorAll('a');
+
+anchorTags.forEach((item) => {
+  item.addEventListener('click', function (event) {
+    console.log(event.target);
+  });
+});
