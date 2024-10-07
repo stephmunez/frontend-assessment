3d2d63cad8d7acc33fdad08cf05fcbf2ff14d39d
Stephen Muñez <sjtmunez@gmail.com>
2024-10-07 18:54:56 +0800

    refactor: modal

--- orchard-fed-test_1/main.js
+++ orchard-fed-test_1/main.js
@@ -6,6 +6,16 @@ const modalImage = document.getElementById('modalImage');
 const modalClose = document.getElementById('modalClose');
 const component1Images = document.querySelectorAll('.component1__image');
 
+const openModal = () => {
+  modal.classList.add('modal--visible');
+  document.body.classList.add('no-scroll');
+};
+
+const closeModal = () => {
+  modal.classList.remove('modal--visible');
+  document.body.classList.remove('no-scroll');
+};
+
 anchorTags.forEach((item) => {
   item.addEventListener('click', function (event) {
     console.log(event.target);
@@ -16,16 +26,23 @@ component1Images.forEach((imageElement) => {
   imageElement.addEventListener('click', (e) => {
     const imgSrc = e.currentTarget.querySelector('img').src;
     modalImage.src = imgSrc;
-    modal.classList.add('modal--visible');
+    openModal();
   });
 });
 
 modalClose.addEventListener('click', () => {
   modal.classList.remove('modal--visible');
+  document.body.classList.remove('no-scroll');
 });
 
 modal.addEventListener('click', (e) => {
   if (e.target === modal) {
-    modal.classList.remove('modal--visible');
+    closeModal();
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeModal();
   }
 });
