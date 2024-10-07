749c212fda4eb1f5c951cf14e511eb1950c4c163
Stephen Muñez <sjtmunez@gmail.com>
2024-10-07 18:49:24 +0800

    feat: image modal

--- orchard-fed-test_1/main.js
+++ orchard-fed-test_1/main.js
@@ -1,9 +1,31 @@
 import 'normalize.css';
 
 const anchorTags = document.querySelectorAll('a');
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modalImage');
+const modalClose = document.getElementById('modalClose');
+const component1Images = document.querySelectorAll('.component1__image');
 
 anchorTags.forEach((item) => {
   item.addEventListener('click', function (event) {
     console.log(event.target);
   });
+});
+
+component1Images.forEach((imageElement) => {
+  imageElement.addEventListener('click', (e) => {
+    const imgSrc = e.currentTarget.querySelector('img').src;
+    modalImage.src = imgSrc;
+    modal.classList.add('modal--visible');
+  });
+});
+
+modalClose.addEventListener('click', () => {
+  modal.classList.remove('modal--visible');
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.classList.remove('modal--visible');
+  }
 });
